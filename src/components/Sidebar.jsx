287dc5160ddc79687162d5d0cf49632593d21854
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-09 14:04:37 -0300

    feat: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -7,12 +8,14 @@ export function Sidebar() {
       src='https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGV2JTIwY29kZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60' />
       
       <div className={styles.profile}>
+        <img src="https://github.com/ViktorHugodev.png" />
         <strong>Victor Hugo</strong>
-        <span>Developer</span>
+        <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar perfil
         </a>
       </footer>
