5cb7aa549cce39f06eb97d7a7d6b70d753a62b54
viktorhugodev <viktorhugo.dev@gmail.com>
2023-03-10 08:53:29 -0300

    feat: Comment component and styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -26,22 +27,26 @@ export function Post() {
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-        <p><a href="#">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p>
+          <a href='#'>jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href=''>#novoprojeto #nlw #rocketseat</a>
+        </p>
       </div>
 
       <form className={styles.form}>
         <strong>Deixe seu feedback</strong>
-        <textarea 
-        placeholder='Deixe um comentário'
-        />
+        <textarea placeholder='Deixe um comentário' />
         <footer>
-        <button
-        type='submit'
-        >Publicar</button>
+          <button type='submit'>Publicar</button>
         </footer>
-   
       </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
